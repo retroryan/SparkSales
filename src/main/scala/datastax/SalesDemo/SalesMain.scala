30a990cd5d05db35e509bda95f9d51055517544e
Ryan Knight <ryan@grandcloud.com>
2015-10-27 22:03:53 -0600

    adding join with cassandra example

--- src/main/scala/datastax/SalesDemo/SalesMain.scala
+++ src/main/scala/datastax/SalesDemo/SalesMain.scala
@@ -1,10 +1,19 @@
 package datastax.SalesDemo
 
-import com.datastax.spark.connector.cql.CassandraConnector
-import org.apache.spark._
 import com.datastax.spark.connector._
+import org.apache.spark._
 
-//  ~/dse-4.8.0/bin/dse spark-submit --class demo.SearchAnalyticsDemo ./target/scala-2.10/SearchAnalyticsDemo-assembly-0.2.0.jar
+// sbt package
+// dse-4.8.0/bin/dse spark-submit --class datastax.SalesDemo.SalesMain ./target/scala-2.10/sparksales_2.10-1.0.jar
+
+case class Stores(store_id: String, minute: Integer)
+
+case class StoresSales(store_id: String, minute: Integer, transaction_id: java.util.UUID, product_id: Integer, quantity: Integer, sale_time: org.joda.time.DateTime) {
+
+  override def toString: String = {
+    s"$store_id, $product_id, $quantity}"
+  }
+}
 
 object SalesMain {
   def main(args: Array[String]) {
@@ -23,25 +32,43 @@ object SalesMain {
     }
   }
 
-  def setup(conf:SparkConf) {
+  def setup(conf: SparkConf) {
 
 
-    CassandraConnector(conf).withSessionDo { session =>
-      session.execute( """CREATE KEYSPACE IF NOT EXISTS search_demo with replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1 }""")
+    /*
+       not needed for this demo but it can be used when a connection to cassandra is needed
 
-      session.execute(
-        """CREATE TABLE IF NOT EXISTS search_demo.kjv (
-          |book text ,
-          |chapter int,
-          |verse int,
-          |body text,
-          |PRIMARY KEY (book, chapter)
-          |)""".stripMargin)
-    }
+        CassandraConnector(conf).withSessionDo { session =>
+         session.execute( """CREATE KEYSPACE IF NOT EXISTS search_demo with replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1 }""")
+
+         session.execute(
+           """CREATE TABLE IF NOT EXISTS search_demo.kjv (
+             |book text ,
+             |chapter int,
+             |verse int,
+             |body text,
+             |PRIMARY KEY (book, chapter)
+             |)""".stripMargin)
+       }*/
   }
 
 
   def runSales(sc: SparkContext): Unit = {
+    val stores = sc.cassandraTable[Stores]("walmartstores", "stores_to_process").where("minute  = ?", 24098450)
+    val joinedSales = stores.joinWithCassandraTable[StoresSales]("walmartstores", "sales_by_store_minute")
+
+    //Two ways to reduce by key - first preserves the types
+    val salesForStoresPairRDD = joinedSales.map {
+      case (store, storeSales) => ((storeSales.store_id, storeSales.product_id), storeSales)
+    }
+
+    val salesByStore = salesForStoresPairRDD.reduceByKey((s1, s2) => s1.copy(quantity = s1.quantity + s2.quantity))
+    salesByStore.collect.foreach(println _)
+
+    //second just returns the raw primitives
+    val storeSalesRDD = joinedSales.map(rows => rows._2)
+    val formattedSales = storeSalesRDD.map(sale => ((sale.store_id, sale.product_id), sale.quantity))
+    val formattedStoreSales = formattedSales.reduceByKey((x, y) => x + y)
 
   }
 }
